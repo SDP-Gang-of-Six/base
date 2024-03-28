9e74aebfd8469976980e471f3eff9300bce5d9ca
I578128 <xinglong.wang@sap.com>
2024-03-28 12:43:26 +0800

    上传图片bug修复

--- src/main/java/cn/wxl475/service/DepartmentService.java
+++ src/main/java/cn/wxl475/service/DepartmentService.java
@@ -1,6 +1,6 @@
 package cn.wxl475.service;
 
-import cn.wxl475.pojo.base.department.Department;
+import cn.wxl475.pojo.base.Department;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface DepartmentService extends IService<Department> {
