01782ae5bd3fbc0633af2605d5d9703770885f64
Cbgogogog <1293543528@qq.com>
2024-04-02 21:35:23 +0800

    feat: 住院疫苗初始化

---
+++ src/main/java/cn/wxl475/controller/HospitalizationController.java
@@ -0,0 +1,9 @@
+package cn.wxl475.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/base/hospitalization")
+public class HospitalizationController {
+}
---
+++ src/main/java/cn/wxl475/service/HospitalizationService.java
@@ -0,0 +1,7 @@
+package cn.wxl475.service;
+
+import cn.wxl475.pojo.base.hospitalization.Hospitalization;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface HospitalizationService extends IService<Hospitalization> {
+}
