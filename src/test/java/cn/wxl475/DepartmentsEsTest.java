d2e991d51da178ec108708ac309de4fd3b8f433b
wxl <1715142548@qq.com>
2024-04-02 16:08:39 +0800

    科室子类

--- src/test/java/cn/wxl475/DepartmentsEsTest.java
+++ src/test/java/cn/wxl475/DepartmentsEsTest.java
@@ -1,6 +1,6 @@
 package cn.wxl475;
 
-import cn.wxl475.pojo.base.Department;
+import cn.wxl475.pojo.base.department.Department;
 import cn.wxl475.repo.DepartmentEsRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
