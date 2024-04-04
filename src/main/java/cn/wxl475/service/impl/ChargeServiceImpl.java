f779674cbaad5a1ce4b66011103b7f3ae345fabe
pc <2528578653@qq.com>
2024-04-04 16:14:29 +0800

    修改illness模块模糊查询接口

--- src/main/java/cn/wxl475/service/impl/ChargeServiceImpl.java
+++ src/main/java/cn/wxl475/service/impl/ChargeServiceImpl.java
@@ -4,6 +4,8 @@ import cn.wxl475.mapper.ChargeMapper;
 import cn.wxl475.pojo.base.Charge.Charge;
 import cn.wxl475.service.ChargeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ChargeServiceImpl extends ServiceImpl<ChargeMapper, Charge> implements ChargeService {
 }
