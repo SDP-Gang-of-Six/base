3461483edcd9f494c0990ab3f74aaf498f825e72
Cbgogogog <1293543528@qq.com>
2024-04-06 23:35:59 +0800

    feat: 分页优化

--- src/main/java/cn/wxl475/controller/HospitalizationController.java
+++ src/main/java/cn/wxl475/controller/HospitalizationController.java
@@ -1,9 +1,20 @@
 package cn.wxl475.controller;
 
+import cn.wxl475.pojo.Result;
+import cn.wxl475.pojo.base.hospitalization.Hospitalization;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/base/hospitalization")
 public class HospitalizationController {
+
+    @PostMapping("create")
+    public Result create(@RequestBody Hospitalization hospitalization){
+
+        return Result.success();
+    }
+
 }
